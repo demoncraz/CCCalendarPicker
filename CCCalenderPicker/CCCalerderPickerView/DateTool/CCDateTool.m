5b0db0f115e93488be5209a3974f3a68cd94a03b
demoncraz <demoncraz@192.168.0.101>
2017-02-15 15:30:07 +0800

    优化了日期算法

--- CCCalenderPicker/CCCalerderPickerView/DateTool/CCDateTool.m
+++ CCCalenderPicker/CCCalerderPickerView/DateTool/CCDateTool.m
@@ -83,32 +83,12 @@
  */
 + (NSInteger)getTotalDaysOfMonth:(NSInteger)month year:(NSInteger)year {
     switch (month) {
-        case 1:
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
             return 31;
         case 2:
             return ((year%4==0)&&(year%100!=0))||(year%400==0) ? 29 : 28;
-        case 3:
-            return 31;
-        case 4:
-            return 30;
-        case 5:
-            return 31;
-        case 6:
-            return 30;
-        case 7:
-            return 31;
-        case 8:
-            return 31;
-        case 9:
-            return 30;
-        case 10:
-            return 31;
-        case 11:
-            return 30;
-        case 12:
-            return 31;
         default:
-            return 0;
+            return 30;
     }
 }
 
